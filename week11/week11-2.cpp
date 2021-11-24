61fc7e66e9e564f76839fb97efad76df991688ad
N1annnn <91170837+N1annnn@users.noreply.github.com>
2021-11-24 11:47:57 +0800

    Add files via upload

---
+++ week11/week11-2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int a[10000000];
+int main()
+{
+    printf("你要幾個質數(最大的數不超過10000000)");
+	int Q;
+	scanf("%d",&Q);
+	int ans=0;
+	for( int i=2 ; ans<Q ;i++)
+	{
+		if ( a[i]==0)
+		{
+		    ans++;
+			printf("%d ",i);
+			for( int k=i+i; k<10000000; k+=i)
+			{
+				a[k]=1;
+			}
+
+		}
+
+	}
+
+}
